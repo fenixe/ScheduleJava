8231d313d8a8ae4a7259de963577a40a82abfd8c
fenixe <fenixe@bigmir.net>
2014-10-05 13:59:58 +0300

    Up
    up

---
+++ src/com/diplom/RulesFields.java
@@ -0,0 +1,37 @@
+package com.diplom;
+
+import com.fasterxml.jackson.annotation.*;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RulesFields {
+    @JsonProperty
+    private String day;
+    @JsonProperty
+    private String lessonId;
+    @JsonProperty
+    private boolean isSet;
+
+    public boolean getIsSet() {
+        return isSet;
+    }
+
+    public void setIsSet(boolean isSet) {
+        this.isSet = isSet;
+    }
+
+    public String getLessonId() {
+        return lessonId;
+    }
+
+    public void setLessonId(String lessonId) {
+        this.lessonId = lessonId;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+}
